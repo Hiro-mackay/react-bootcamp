4bd549fa5363b592f62bd98307c751befc62e5c9
Hiro-mackay <johnmackay150@gmail.com>
2021-08-26 03:08:41 +0900

    codegen に schema を追加

--- script/codegen.js
+++ script/codegen.js
@@ -1,5 +1,12 @@
 module.exports = {
-  schema: {},
+  // 追記
+  schema: {
+    [process.env.REACT_APP_GRAPHQL_END_POINT_ORIGIN]: {
+      headers: {
+        "x-hasura-admin-secret": process.env.REACT_APP_HASURA_SECRET_KEY,
+      },
+    },
+  },
   documents: "",
   generates: {},
 };
