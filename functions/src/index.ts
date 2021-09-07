08ae336a999944d9fc63f8c7e62b1e20dafb7ae5
Mackay <johnmackay150@gmail.com>
2021-09-07 23:29:22 +0900

    firebase functionsを設定してコードを記述する

---
+++ functions/src/index.ts
@@ -0,0 +1,23 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+// firebaseの初期化
+admin.initializeApp(functions.config().firebase);
+
+/**
+ * Firebase FunctionsにprocessSignUpと言う名前の関数を作成
+ * `functions.auth.user().onCreate`で、Authenticationでuserが作成された時に実行される関数を定義
+ */
+exports.processSignUp = functions.auth.user().onCreate((user) => {
+  // Hasura用のカスタムクレームの作成
+  const customClaims = {
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "user",
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": user.uid,
+    },
+  };
+
+  // userのトークンにカスタムクレームを追加する
+  return admin.auth().setCustomUserClaims(user.uid, customClaims);
+});
