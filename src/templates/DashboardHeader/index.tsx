6771da8af277adae214f1440af2bad9b97ccaeb8
Mackay <johnmackay150@gmail.com>
2021-09-07 20:17:08 +0900

    ログイン状態でヘッダーのデザインを変更する

--- src/templates/DashboardHeader/index.tsx
+++ src/templates/DashboardHeader/index.tsx
@@ -1,93 +1,54 @@
-import {
-  AppBar,
-  Avatar,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@material-ui/core";
+import { AppBar, Avatar, Button, IconButton, Toolbar } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
 import { Logo } from "../../components/Logo";
 import { SearchBar } from "./SearchBar";
-
-// export defaultしているので、import側でuseStylesと命名します。
-// 命名はなんでも構いませんが、一貫して全て同じ名前にすることで、カスタム用のCSSを使用していることを明示します。
 import useStyles from "./style";
-
-// codegenで生成したコードをimportします。
-import { useUserByIdQuery } from "../../utils/graphql/generated";
-import { useEffect } from "react";
-
 import { Link } from "react-router-dom";
 
+// 追加
+import { useRecoilValue } from "recoil";
+import { GlobalUser } from "../../stores/User";
+
 export const DashboardHeader = () => {
-  // 一度、useStylesを実行して、CSSを生成します。
   const styles = useStyles();
 
-  //GraphQLの`Query`を発行して、Hasuraのエンドポイントにリクエストを飛ばし、返り値を取得するまでが、この3行に詰まっています。
-  const { data, error } = useUserByIdQuery({
-    variables: { id: "testid" },
-  });
-
-  useEffect(() => {
-    console.log(data);
-    console.log(error);
-  }, [data]);
+  // ユーザー情報Atom
+  const globalUser = useRecoilValue(GlobalUser);
 
   return (
-    // color="inherit" : 背景を白色に
-    // elevation={0} : 影(box-shadow)を無くす
     <AppBar elevation={0} color="inherit">
-      {/* 
-        <Toolbar>に"justifyContent: "space-between"のCSSを追加
-      */}
       <Toolbar className={styles.between}>
-        {/* 
-          <IconButton>とLogoを<div>で囲み、<div>にflexを付与
-        */}
         <div className={styles.flex}>
           <IconButton>
             <MenuIcon />
           </IconButton>
-          {/* 
-          "useStyles"の値は、CSSモジュールと全く同じような使い方で、使用すすることができます。
-          例えば、仮に、後になって"Material-UI"をやめて独自のデザインを取り入れた時も、CSSモジュールでリプレイスしやすいような形にしています。
-        */}
           <Link to="/" className={styles.logo}>
             <Logo />
           </Link>
         </div>
 
-        {/* 
-          検索バーの表示
-        */}
         <SearchBar />
 
-        {/* 
-          2つの<IconButton>を<div>で囲み、<div>にflexを付与
-        */}
         <div className={styles.flex}>
           {/*
-            データが取得されたら、`data`内に`Schema`と同じ名前のオブジェクトの中にデータが格納されます。
-            データがないときは表示されません。
+            ユーザーがログインしていれば、ユーザー用のデザインを表示
+            未ログインであれば「ログインボタン」を表示
           */}
-          <IconButton>
-            <Typography>{data?.users_by_pk?.name}</Typography>
-          </IconButton>
-
-          {/* 
-          新規動画作成のアイコンボタンを追加
-        */}
-          <IconButton>
-            <VideoCallIcon />
-          </IconButton>
-
-          {/* 
-          プロフィールアイコンを追加
-        */}
-          <IconButton className={styles.profileIcon}>
-            <Avatar />
-          </IconButton>
+          {globalUser ? (
+            <>
+              <IconButton>
+                <VideoCallIcon />
+              </IconButton>
+              <IconButton className={styles.profileIcon}>
+                <Avatar />
+              </IconButton>
+            </>
+          ) : (
+            <Button variant="outlined" color="primary" href="/login">
+              ログイン
+            </Button>
+          )}
         </div>
       </Toolbar>
     </AppBar>
