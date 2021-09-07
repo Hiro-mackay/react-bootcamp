bd248c3644cd513379293032f744eb16b718a2a5
Mackay <johnmackay150@gmail.com>
2021-09-07 20:14:27 +0900

    AuthStateListenerでcredentialを取得

--- src/providers/AuthStateListener/index.tsx
+++ src/providers/AuthStateListener/index.tsx
@@ -1,12 +1,26 @@
 import { useEffect } from "react";
 import { PropsWithChildren } from "react";
+import { useSetRecoilState } from "recoil";
+import { AuthCredential } from "../../stores/AuthCredential";
+import { AuthCredentialLoaded } from "../../stores/AuthCredentialLoaded";
 import { fireAuth } from "../../utils/Firebase/config";
 
 export const AuthStateListener = ({ children }: PropsWithChildren<{}>) => {
+  const setCredential = useSetRecoilState(AuthCredential);
+  const setLoaded = useSetRecoilState(AuthCredentialLoaded);
+
   useEffect(() => {
     const unsubscriber = fireAuth.onAuthStateChanged(async (credential) => {
-      const uid = credential?.uid;
+      // uidが存在→つまり認証が済んでいるユーザーであればuidを格納する
+      setCredential(credential?.uid || undefined);
+
+      // onAuthStateChangedが呼ばれたのでtrueをセット
+      setLoaded(true);
     });
+
+    // これはonAuthStateChangedを停止する用の関数
+    // useEffectの返り値に関数を指定すると、ReactはそのuseEffectがアンマウントされた時、つまりコンポーネントが表示されなくなったらreturnに指定された関数を実行します。
+    // この場合、AuthStateListenerはProviderとしてアプリケーションのRootで呼んでいるのでアプリケーションを閉じたときに実行される
     return unsubscriber;
   });
   return <>{children}</>;
