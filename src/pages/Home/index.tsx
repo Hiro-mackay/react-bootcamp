119e2fb124a6940d16a02fe3273121da12b78cbc
Hiro-mackay <johnmackay150@gmail.com>
2021-09-08 03:48:06 +0900

    動画リストを読み込む

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,49 @@
 import { Container, Grid } from "@material-ui/core";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { VideoCard } from "../../components/VideoCard";
+import { storage } from "../../utils/Firebase/config";
+import { useVideosQuery } from "../../utils/graphql/generated";
 
 export const Home = () => {
+  // videoを取得する`query`
+  const { data, error } = useVideosQuery();
+
+  // エラーがあればコンソールの表示
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
   return (
     // 全ての要素をContainerで囲むことで、デザインが「整う」
     <Container>
       <Grid container spacing={2}>
-        <Grid item xs={3}>
-          <VideoCard />
-        </Grid>
-
-        <Grid item xs={3}>
-          <VideoCard />
-        </Grid>
-        <Grid item xs={3}>
-          <VideoCard />
-        </Grid>
-        <Grid item xs={3}>
-          <VideoCard />
-        </Grid>
-        <Grid item xs={3}>
-          <VideoCard />
-        </Grid>
+        {/*
+          `query`で取得した動画データを表示する
+        */}
+        {data?.videos.map((video) => (
+          <Grid item xs={3}>
+            {/*
+              カードをクリックしたら、プレイヤー画面を表示します。
+            */}
+            <Link to={`/watch/${video.id}`} style={{ textDecoration: "none" }}>
+              {/*
+                `<VideoCard>`には、先ほど指定されていたpropsを流し込みます
+              */}
+              <VideoCard
+                title={video.title}
+                // ownerは投稿者の名前を入れたいが、現段階では、名前を取得することができない
+                owner={video.owner_id}
+                views={video.views}
+                created={video.created_at}
+                // <VideoCard> で非同期的に画像を取得するための関数
+                fetcher={() =>
+                  storage.ref(video.thumbnail_url).getDownloadURL()
+                }
+              />
+            </Link>
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
