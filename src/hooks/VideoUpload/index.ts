23ec5e06a4256a150835a332acda98cfc21710da
Hiro-mackay <johnmackay150@gmail.com>
2021-09-08 03:28:43 +0900

    useVideoUploadにGraphQL処理を追加

--- src/hooks/VideoUpload/index.ts
+++ src/hooks/VideoUpload/index.ts
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { storage } from "../../utils/Firebase/config";
 import { v4 as uuidv4 } from "uuid";
+import { useInsertVideoMutation } from "../../utils/graphql/generated";
+import { useRecoilValue } from "recoil";
+import { GlobalUser } from "../../stores/User";
 
 type UploadProps = {
   file: {
@@ -16,6 +19,12 @@ export const useVideoUpload = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error>();
 
+  // 動画のメタデータを保存するGraphQLMutation
+  const [mutation, { error: apolloError }] = useInsertVideoMutation();
+
+  // `video`の`ownerId`のために、userのidを取得する
+  const user = useRecoilValue(GlobalUser);
+
   // Firebase Storageにファイルをアップロードする処理
   const uploadStorage = (id: string, file: File, path: string) => {
     // ファイルから拡張子を抜き出す
@@ -30,12 +39,18 @@ export const useVideoUpload = () => {
   };
 
   const upload = async ({ file, title, description, ownerId }: UploadProps) => {
+    // ユーザーが読み込まれていない、未ログインであれば処理を中断する
+    if (!user?.id) {
+      return;
+    }
+
     // 処理が始まったら、ローディング中にする
     setLoading(true);
 
     // 動画とサムネイルのそれぞれのuuidを生成する
     const videoName = uuidv4();
     const thumbName = uuidv4();
+    const videoId = uuidv4();
 
     // try-catch構文でPromise(アップロード処理)のエラーをキャッチする
     try {
@@ -54,6 +69,21 @@ export const useVideoUpload = () => {
         file.thumbnail,
         "thumbnails"
       );
+
+      // 動画のメタデータを保存する
+      const res = await mutation({
+        variables: {
+          id: videoId,
+          title,
+          description,
+          video_url: videoUploadTask.ref.fullPath,
+          thumbnail_url: thumbnailUploadTask.ref.fullPath,
+          owner_id: ownerId,
+        },
+      });
+
+      // 全ての処理が終わったら、動画のメタデータを返す
+      return res.data?.insert_videos_one;
     } catch (error) {
       // アップロードの途中でエラーが発生したら、処理を中断して、ここに記述される処理が行われる
 
@@ -64,6 +94,14 @@ export const useVideoUpload = () => {
       setLoading(false);
     }
   };
+
+  // Apollo Clientのエラーをキャッチする
+  useEffect(() => {
+    if (apolloError) {
+      console.error(apolloError);
+      setError(new Error("エラーが発生しました。最初からやり直してください。"));
+    }
+  }, [apolloError]);
 
   return {
     upload,
