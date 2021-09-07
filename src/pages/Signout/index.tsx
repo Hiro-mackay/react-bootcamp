e7444a783aaa52c7d6851b2392deb40fac903b3f
Mackay <johnmackay150@gmail.com>
2021-09-07 19:42:09 +0900

    SignoutコンポーネントにHooksを追加

--- src/pages/Signout/index.tsx
+++ src/pages/Signout/index.tsx
@@ -1,7 +1,17 @@
 import { Card, CircularProgress, Typography } from "@material-ui/core";
+import { useEffect } from "react";
+import { useSignout } from "../../hooks/Authentication/useSignout";
 import useStyles from "./style";
+
 export const Signout = () => {
   const styles = useStyles();
+  const { signout } = useSignout();
+
+  // ページを表示したタイミングで、signout処理を実行するための処理
+  useEffect(() => {
+    signout();
+  });
+
   return (
     <Card className={styles.root} variant="outlined">
       <CircularProgress className={styles.margin} size={70} thickness={4} />
