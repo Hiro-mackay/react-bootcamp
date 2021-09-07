5f3148922b31766b9d8c237068fde1c3ce42f27b
Hiro-mackay <johnmackay150@gmail.com>
2021-09-08 03:43:48 +0900

    SubHeaderContentでpropsを表示できるようにする

--- src/components/VideoCard/SubHeaderContent/index.tsx
+++ src/components/VideoCard/SubHeaderContent/index.tsx
@@ -1,10 +1,39 @@
 import { Typography } from "@material-ui/core";
 
-export const SubHeaderContent = () => {
+export type SubHeaderContentProps = {
+  owner: string;
+  views: number;
+  created: Date;
+};
+
+// 親コンポーネントから、投稿者情報、再生回数、アップロード日時を受け取ります。
+export const SubHeaderContent = ({
+  owner,
+  views,
+  created,
+}: SubHeaderContentProps) => {
   return (
     <>
-      <Typography variant="body2">Figma</Typography>
-      <Typography variant="body2">16K views 2 months ago</Typography>
+      <Typography variant="body2">
+        {
+          // 追加
+          // 投稿者情報
+          owner
+        }
+      </Typography>
+      <Typography variant="body2">
+        {
+          // 追加
+          // 再生回数
+          views
+        }
+        views
+        {
+          // 追加
+          // 投稿時間を表示
+          new Date(created).toLocaleDateString()
+        }
+      </Typography>{" "}
     </>
   );
 };
