fe2fa6da75c449c95e82f3899c5092c518c88eb4
Mackay <johnmackay150@gmail.com>
2021-09-07 19:25:31 +0900

    useAuthHelper処理の詳細

--- src/hooks/Authentication/useAuthHelper/index.ts
+++ src/hooks/Authentication/useAuthHelper/index.ts
@@ -7,7 +7,19 @@ import { useState } from "react";
 // password: Password入力フォームに関するエラー
 export type ErrorState = "main" | "name" | "email" | "password";
 
-export const useAuthHelper = () => {
+export type SetErrorFn = (name: ErrorState, message: string) => void;
+
+/**
+ * useAuthHelper()をするときに、呼び出し元からそれぞれの処理の実態を注入する
+ *
+ * @param executeProcess 実際に認証処理の実態を外部から追加する
+ * @param formValidation バリデーションチェックの実態を外部から追加する
+ * @returns
+ */
+export const useAuthHelper = (
+  executeProcess: () => Promise<void>,
+  formValidation: (setError: SetErrorFn) => boolean
+) => {
   // 複数のエラーを同時に管理できるようにするためのstate
   // Mapは { key : value }の形でオブジェクトを管理できるJavascriptのデータ構造
   // ただのObjectと違い、便利なメソッドが用意されている。
@@ -16,15 +28,35 @@ export const useAuthHelper = () => {
   // ローディング処理も合わせて共通化
   const [loading, setLoading] = useState<boolean>(false);
 
-  const setErrorHandler = () => {
-    // エラーをセットする
+  const setErrorHandler: SetErrorFn = (name, message) => {
+    setError((prev) => new Map(prev.set(name, message)));
   };
 
   const authExecute = async () => {
+    // 認証を実行したら、一度エラー文をリセットする
+    // これをしないと不要なエラーメッセージが画面に表示されたままになってしまう。
+    setError(new Map());
+
     // バリデーションの確認を行う
-    // バリデーションが問題なければ、認証ロジックを実行
-    // 成功すれば、リダイレクト処理
-    // エラーがあれば、エラーをセットして処理を中断
+    const invalidValidation = formValidation(setErrorHandler);
+
+    // バリデーションが問題あれば処理を中断
+    if (invalidValidation) return;
+
+    // 処理が開始したらローディング中
+    setLoading(true);
+
+    try {
+      // 認証ロジックを実行
+      // 成功すれば、リダイレクト処理（この処理はここでは書いてありません。）
+      await executeProcess();
+    } catch (error: any) {
+      // エラーがあれば、エラーをセットして処理を中断
+      setErrorHandler("main", error.message);
+    } finally {
+      // 処理が終了したら、ローディングはfalse
+      setLoading(false);
+    }
   };
 
   return {
