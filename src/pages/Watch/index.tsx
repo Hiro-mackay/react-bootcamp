04217bf9a0f7a6a92a59f710fc95cec2dd7f54ee
Hiro-mackay <johnmackay150@gmail.com>
2021-09-08 04:42:29 +0900

    動画再生コンポーネントを完成

--- src/pages/Watch/index.tsx
+++ src/pages/Watch/index.tsx
@@ -2,9 +2,37 @@ import { Container, Grid } from "@material-ui/core";
 import { VideoPlayerCard } from "./VideoPlayerCard";
 import useStyles from "./style";
 import { VideoHorizontalCard } from "../../components/VideoHorizontalCard";
+import { useParams } from "react-router";
+import {
+  useRecommendVideosQuery,
+  useVideoByPkQuery,
+} from "../../utils/graphql/generated";
+import { storage } from "../../utils/Firebase/config";
+import { Link } from "react-router-dom";
 
 export const Watch = () => {
   const styles = useStyles();
+
+  // 追加
+  // URLから再生する動画のIDを取得する
+  const { videoId } = useParams();
+
+  // 追加
+  // 再生する動画を取得する
+  const { data: currentVideo } = useVideoByPkQuery({
+    variables: {
+      id: videoId,
+    },
+  });
+
+  // 追加
+  // リコメンドの動画を取得する
+  const { data: recommendVides } = useRecommendVideosQuery({
+    variables: {
+      currentVideoId: videoId,
+    },
+  });
+
   return (
     // 全体のデザインを整えるためのコンテナー
     // 詳細：https://material-ui.com/ja/components/container/
@@ -20,22 +48,55 @@ export const Watch = () => {
           例えば、下記は全体を"12"とした場合の、比率が"9:3"となるようにカラムの幅を指定している。
         */}
         <Grid item xs={8}>
-          <VideoPlayerCard />
+          {/*
+          再生する動画の情報を渡す
+        */}
+          <VideoPlayerCard
+            title={currentVideo?.videos_by_pk?.title}
+            description={currentVideo?.videos_by_pk?.description}
+            views={currentVideo?.videos_by_pk?.views}
+            ownerName={currentVideo?.videos_by_pk?.owner?.name}
+            date={currentVideo?.videos_by_pk?.created_at}
+            fetcher={async () => {
+              if (currentVideo?.videos_by_pk?.video_url) {
+                return storage
+                  .ref(currentVideo.videos_by_pk.video_url)
+                  .getDownloadURL();
+              }
+              return undefined;
+            }}
+          />
         </Grid>
-        <Grid item xs={4}>
-          <div className={styles.cardPadding}>
-            <VideoHorizontalCard />
-          </div>
-          <div className={styles.cardPadding}>
-            <VideoHorizontalCard />
-          </div>
-          <div className={styles.cardPadding}>
-            <VideoHorizontalCard />
-          </div>
-          <div className={styles.cardPadding}>
-            <VideoHorizontalCard />
-          </div>
-        </Grid>
+        {/*
+          追加
+          リコメンドの動画を一覧表示
+        */}
+        {recommendVides?.videos.map((video) => (
+          <Grid item xs={4}>
+            <div className={styles.cardPadding}>
+              {/*
+                動画プレイヤーを表示するためのリンク
+              */}
+              <Link
+                to={`/watch/${video.id}`}
+                style={{ textDecoration: "none" }}
+              >
+                {/*
+                  カードの表示に必要なデータをpropsに渡す
+                */}
+                <VideoHorizontalCard
+                  title={video.title}
+                  views={video.views}
+                  owner={video.owner?.name || ""}
+                  created={video.created_at}
+                  fetcher={() =>
+                    storage.ref(video.thumbnail_url).getDownloadURL()
+                  }
+                />
+              </Link>
+            </div>
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
