ea813af74e3513f94ce194fa4e71382cb62c5101
Hiro-mackay <johnmackay150@gmail.com>
2021-09-08 12:53:18 +0900

    signup コンポーネントに hooks を実装

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -2,24 +2,51 @@ import { Button, Card, TextField, Typography } from "@material-ui/core";
 import { Logo } from "../../components/Logo";
 import useStyles from "./style";
 
+// import
+import { useSignup } from "../../hooks/Authentication/useSignup";
+
 export const Signup = () => {
   const styles = useStyles();
+
+  // useSignup Hooksを使用
+  const { ref, error, loading, signup } = useSignup();
+
   return (
     <Card className={styles.root} variant="outlined">
       {/* ロゴコンポーネント */}
       <div className={`${styles.logo} ${styles.margin}`}>
         <Logo />
       </div>
-
       {/* タイトルコンポーネント */}
       <Typography className={styles.margin} component="h1" variant="h5">
         新規アカウント登録
       </Typography>
 
+      {/*
+        エラーメッセージを表示
+        ErrorをMapで管理しているので、簡単にエラーがあるかどうかを確認できる
+        */}
+      {error.has("main") && (
+        <Typography className={styles.margin} color="error">
+          {error.get("main")}
+        </Typography>
+      )}
+
       {/* 名前フィールド */}
       <label className={`${styles.label} ${styles.margin}`}>
         <Typography>名前</Typography>
-        <TextField required size="small" fullWidth variant="outlined" />
+        <TextField
+          required
+          size="small"
+          fullWidth
+          variant="outlined"
+          // useRefで作成したnameRefを渡してフォームの値を取得する。
+          inputRef={ref.nameRef}
+          // エラーがあれば、フォームのデザインをerror用に変更させる
+          error={error.has("name")}
+          // エラーの詳細のフォームの下部に表示する
+          helperText={error.has("name") ? error.get("name") : ""}
+        />
       </label>
 
       {/* メールアドレスフィールド */}
@@ -31,6 +58,12 @@ export const Signup = () => {
           size="small"
           fullWidth
           variant="outlined"
+          // useRefで作成したemailRefを渡してフォームの値を取得する。
+          inputRef={ref.emailRef}
+          // エラーがあれば、フォームのデザインをerror用に変更させる
+          error={error.has("email")}
+          // エラーの詳細のフォームの下部に表示する
+          helperText={error.has("email") ? error.get("email") : ""}
         />
       </label>
 
@@ -43,18 +76,34 @@ export const Signup = () => {
           size="small"
           fullWidth
           variant="outlined"
+          // useRefで作成したemailRefを渡してフォームの値を取得する。
+          inputRef={ref.passwordRef}
+          // エラーがあれば、フォームのデザインをerror用に変更させる
+          error={error.has("password")}
+          // エラーの詳細のフォームの下部に表示する
+          helperText={error.has("password") ? error.get("password") : ""}
         />
       </label>
 
-      {/* Submitボタン */}
       <div className={styles.margin}>
-        <Button variant="contained" color="primary">
-          新規作成
+        <Button
+          variant="contained"
+          color="primary"
+          // ローディング中はボタンを押せないようにする
+          disabled={loading}
+          // ボタンをクリックしたら認証処理を実行する
+          onClick={signup}
+        >
+          {/* ローディング中のテキストを変更する */}
+          {loading ? "アカウント作成中" : "新規作成"}
         </Button>
       </div>
 
+      {/*
+        ついでに、ログイン画面にリダイレクトできるようにリンクを入れましょう。
+      */}
       <div>
-        <Button href="#link" color="primary">
+        <Button href="/login" color="primary">
           ログインはこちら
         </Button>
       </div>
